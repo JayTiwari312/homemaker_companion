48cecff9a46bd3311b3b0eb0b415c8a191f1e1c5
Vatsal2000 <vatsaldelhi@gmail.com>
2020-05-20 23:29:58 +0530

    Added the list functionality

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:homemakercompanion/items.dart';
 import 'constants.dart';
 
 class ListScreen extends StatefulWidget {
@@ -10,6 +12,19 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
+  List<item> items = [
+    item(item_name: "chocolate", quantity: 2, image: 'images/chocolate.jpeg'),
+    item(item_name: "Milk", quantity: 2, image: 'images/milk.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+    item(item_name: "Rice", quantity: 2, image: 'images/rice.jpeg'),
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +36,44 @@ class _ListScreenState extends State<ListScreen> {
         title: Center(child: Text('List')),
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: Center(
-        child: Text(
-          'List Screen',
-          style: TextStyle(fontSize: 50.0),
-        ),
-      ),
-    );
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index){
+          return Card(
+            child: new Container(
+              child: new Row(
+                children: <Widget>[
+                  new CircleAvatar(backgroundImage: AssetImage(items[index].image),),
+                  new Text(items[index].item_name),
+                  new RaisedButton(
+                    onPressed: (){
+                      setState(() {
+                        if(items[index].quantity>0){
+                          items[index].quantity--;
+                        }
+                      });
+                    },
+                    child: Text('-'),
+                  ),
+                  new Text((items[index].quantity.toString())),
+                  new RaisedButton(
+                    onPressed: (){
+                      setState(() {
+                        items[index].quantity++;
+                      });
+                    },
+                    child: Text('+'),
+                  ),
+                ],
+              ),
+            ),
+
+          );
+        }
+      )
+
+        );
   }
 }
+
+
